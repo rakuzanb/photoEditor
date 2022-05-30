7abd7c2aa24786f51442fa5f3812862cd7402ade
6b <99634486+rakuzanb@users.noreply.github.com>
2022-05-30 10:04:38 +0600

    Update beks.py

--- beks.py
+++ beks.py
@@ -1,42 +1,39 @@
+
 from glob import glob
 import tkinter as tk
 from tkinter.constants import ANCHOR
-from PIL import Image
-from PIL import ImageTk
-from PIL import ImageEnhance
-from tkinter import filedialog
-import cv2
-import numpy as np
 
-def yellowButton_callback():
-    opencvImage = cv2.cvtColor(np.array(originalImage), cv2.COLOR_RGB2BGR)
-    opencvImage[:, :, 0] - 20
-    global outputImage
-    outputImage = Image.fromarray(cv2.cvtColor(opencvImage, cv2.COLOR_BGR2RGB))
-    dispayImage(outputImage)
 
-def blueButton_callback():
+
+def blue_callback():
     opencvImage = cv2.cvtColor(np.array(originalImage), cv2.COLOR_RGB2BGR)
     opencvImage[:, :, 2] - 100
     global outputImage
     outputImage = Image.fromarray(cv2.cvtColor(opencvImage, cv2.COLOR_BGR2RGB))
     dispayImage(outputImage)
     
-def pinkButton_callback():
+def pink_callback():
     opencvImage = cv2.cvtColor(np.array(originalImage), cv2.COLOR_RGB2BGR)
     opencvImage[:, :, 1] - 100
     global outputImage
     outputImage = Image.fromarray(cv2.cvtColor(opencvImage, cv2.COLOR_BGR2RGB))
     dispayImage(outputImage)
+def yellow_callback():
+    opencvImage = cv2.cvtColor(np.array(originalImage), cv2.COLOR_RGB2BGR)
+    opencvImage[:, :, 0] - 20
+    global outputImage
+    outputImage = Image.fromarray(cv2.cvtColor(opencvImage, cv2.COLOR_BGR2RGB))
+    dispayImage(outputImage)
+
     
-def orangeButton_callback():
+def orange_callback():
     opencvImage = cv2.cvtColor(np.array(originalImage), cv2.COLOR_RGB2BGR)
     opencvImage[:, :, 2] - 200
     global outputImage
     outputImage = Image.fromarray(cv2.cvtColor(opencvImage, cv2.COLOR_BGR2RGB))
     dispayImage(outputImage)
     
-def noneButton_callback():
+def none_callback():
     pass
 
 def dispayImage(displayImage):
@@ -46,17 +43,17 @@ def dispayImage(displayImage):
     showWindow.photo_ref = ImagetoDisplay
     showWindow.pack()
     
-def importButton_callback():
+def import_callback():
     global originalImage
     filename = filedialog.askopenfilename()
     originalImage = Image.open(filename)
     dispayImage(originalImage)
     
-def saveButton_callback():
+def save_callback():
     savefile = filedialog.asksaveasfile(defaultextension=".jpg")
     outputImage.save(savefile)
     
-def closeButton_callback():
+def close_callback():
     window.destroy()
     
 def brightness_callback(brightness_pos):
@@ -103,13 +100,13 @@ Frame3.pack(anchor=tk.NW)
 welcome = tk.Label(Frame1, text="WELCOME TO YOUR OWN IMAGE EDITOR", font=("ariel 12 bold"),pady=5, padx=407, bg="#8EESEE")
 welcome.grid(row=0, column=2)
 
-importButton = tk.Button(Frame1, bg="#FFD700", text="Import", padx=10, pady=5, command=importButton_callback)
+importn = tk.Button(Frame1, bg="#FFD700", text="Import", padx=10, pady=5, command=import_callback)
 importButton.grid(row=0, column=0)
 
-saveButton = tk.Button(Frame1, bg="#FFD700", text="Save", padx=10, pady=5, command=saveButton_callback)
+saveButton = tk.Button(Frame1, bg="#FFD700", text="Save", padx=10, pady=5, command=save_callback)
 saveButton.grid(row=0, column=1)
 
-closeButton = tk.Button(Frame1, bg="#FFD700", text="Close", padx=32, pady=5, command=closeButton_callback)
+closeButton = tk.Button(Frame1, bg="#FFD700", text="Close", padx=32, pady=5, command=close_callback)
 closeButton.grid(row=0, column=3)
 
 brightnessSlider = tk.Scale(Frame2, bg="#8ED2C9", fg="black", troughcolor="#006495", label="Brightness", from_=0, to=2, orient=tk.HORIZONTAL, length=screen_width,
